4faa2c1b20f86f5f85c055ea228912fdb33f8b96
Noinkin <87577662+Noinkin@users.noreply.github.com>
2022-05-10 19:01:12 +0100

    Dev - Update Error System, will be updated further.
    Updated Error System, added lib, will add more soon.
    
    Dev Branch

--- events/error.js
+++ events/error.js
@@ -1,6 +1,7 @@
 const config = require('../config.json')
+const lib = require(`${process.cwd()}/lib`)
 
 module.exports = async (client, error) => {
-    console.log("Error happend: "+error)
+    lib.consoleLogError({ message: 'error' })
     client.channels.cache.get(config.channelID.errorLog).send(''+error+'.')
 }
